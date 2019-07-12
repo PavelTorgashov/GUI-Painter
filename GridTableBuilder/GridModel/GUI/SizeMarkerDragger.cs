06bab888c19259909fb873cff17fcdbb36174ad7
ashsvis <ashsvis@gmail.com>
2019-07-12 09:39:35 +0400

    resizing of memorized points made

--- GridTableBuilder/GridModel/GUI/SizeMarkerDragger.cs
+++ GridTableBuilder/GridModel/GUI/SizeMarkerDragger.cs
@@ -14,7 +14,16 @@ namespace GridTableBuilder.GridModel.GUI
         SizeMarker marker;
         List<Node> nodes;
         MouseController mc;
-        PointF topLeft;
+        PointF topLeft, bottomRight;
+        SizeF size;
+
+        class NodeInfo
+        {
+            public PointF OriginalLocation { get; set; }
+            public PointF Offset { get; set; }
+            public Node Node { get; set; }
+        }
+        List<NodeInfo> infos;
 
         public SizeMarkerDragger(SizeMarker marker)
         {
@@ -26,8 +35,13 @@ namespace GridTableBuilder.GridModel.GUI
             this.mc = mc;
 
             //get my node list
-            nodes = marker.Grid.Nodes.ToList();
-            topLeft = new PointF(nodes.Min(n => n.OriginalLocation.X), nodes.Min(n => n.OriginalLocation.Y));
+            infos = new List<NodeInfo>();
+            foreach (var node in marker.Grid.Nodes)
+                infos.Add(new NodeInfo() { Node = node, OriginalLocation = node.OriginalLocation, Offset = node.Offset });
+
+            topLeft = new PointF(infos.Min(n => n.OriginalLocation.X), infos.Min(n => n.OriginalLocation.Y));
+            bottomRight = new PointF(infos.Max(n => n.OriginalLocation.X), infos.Max(n => n.OriginalLocation.Y));
+            size = new SizeF(bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y);
 
             mc.MouseMove += Mc_MouseMove;
             mc.MouseUp += Mc_MouseUp;
@@ -40,27 +54,25 @@ namespace GridTableBuilder.GridModel.GUI
 
         private void Mc_MouseMove(MouseEventArgs e)
         {
-            const float padding = 12;
+            float padding = marker.Offset * 2;
             var p = new PointF(e.Location.X, e.Location.Y);
-
             var dx = p.X - marker.Location.X;
             var dy = p.Y - marker.Location.Y;
-            marker.Location.X = p.X;
-            marker.Location.Y = p.Y;
 
-            var bottomRight = new PointF(nodes.Max(n => n.OriginalLocation.X), nodes.Max(n => n.OriginalLocation.Y));
-            var size = new SizeF(bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y);
-            if (size.Width < padding && dx < 0 || size.Height < padding && dy < 0)
+            var width = e.Location.X - topLeft.X - padding;
+            var height = e.Location.Y - topLeft.Y - padding;
+            if (width < padding && dx < 0 || height < padding && dy < 0)
                 return;
+
             var kx = (size.Width + dx) / size.Width;
             var ky = (size.Height + dy) / size.Height;
-            foreach (var node in nodes)
+            foreach (var info in infos)
             {
-                var pt = new PointF(node.OriginalLocation.X - topLeft.X, node.OriginalLocation.Y - topLeft.Y);
+                var pt = new PointF(info.OriginalLocation.X - topLeft.X, info.OriginalLocation.Y - topLeft.Y);
                 if (pt.X < 0) pt.X = 0;
                 if (pt.Y < 0) pt.Y = 0;
-                node.OriginalLocation = new PointF(topLeft.X + pt.X * kx, topLeft.Y + pt.Y * ky);
-                node.Offset = new PointF(node.Offset.X * kx, node.Offset.Y * ky);
+                info.Node.OriginalLocation = new PointF(topLeft.X + pt.X * kx, topLeft.Y + pt.Y * ky);
+                info.Node.Offset = new PointF(info.Offset.X * kx, info.Offset.Y * ky);
             }
         }
 
